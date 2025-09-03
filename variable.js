f3d4e7d4300a43c5f8b59702a37f775fb4164660
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-03 11:24:15 +0530

    project complete

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountId = 87765
+let accountGmail ="Happy@gmail.com"
+var accountPassword="2003"
+accountCity="Bhubneswar"
+let accountState
+
+
+accountPassword="776655"
+accountGmail="manoranjan@gmail.com"
+accountCity="Khandagiri"
+console.log(accountId,accountGmail,accountPassword);
+
+console.table([accountPassword,accountGmail,accountCity,accountState])
+
