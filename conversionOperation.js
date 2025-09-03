0d6b7ec7046f89aa50949e0411450cf51afa1c69
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-03 13:13:12 +0530

    operations

--- conversionOperation.js
+++ conversionOperation.js
@@ -24,7 +24,47 @@ let booleanLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
 
 // 33=> string
+//*****************************OPERATIONS***********************************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+
+
+let str1 = "hello"
+let str2 = "Happy"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");  // 32
+
+// console.log((3+4) * 5 * 3);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 200
+gameCounter--;
+console.log(gameCounter);
+
+
+
+
+
+
+
