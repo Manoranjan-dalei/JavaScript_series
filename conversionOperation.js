8525d9b2c58c20425e6674ac2e06c7f2dec0152a
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-03 12:19:04 +0530

    conversionOperation

---
+++ conversionOperation.js
@@ -0,0 +1,30 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let  valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+//"33"=>Number
+//"33abc"=>NaN  (Not a number)
+// true = 1; false = 0;
+
+let isLoggedIn = "Happy"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn);
+
+//1=> true;   0=>false
+// "" => false
+// "Happy" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+// 33=> string
