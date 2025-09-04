ffee5df0cca33e27ba0549e1a19d11977fa3386f
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-04 14:46:10 +0530

    dataTypes_summery

--- dataTypes_summery.js
+++ dataTypes_summery.js
@@ -13,3 +13,24 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id === anotherId);
+
+//const bigNumber = 34567846573697666n
+
+//Reference (Non-Primitive)
+
+//Array,Object,Functions
+
+const Heros=["saktiman","Avengers","Ironman"]
+
+let heros = {
+    name :"happy",
+    age : 22
+}
+
+const myFuntion = function(){
+    console.log("Hello world");
+    
+}
+
+console.log( typeof bigNumber);
+
