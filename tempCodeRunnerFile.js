029327bbee594d62dd3d27f9500579bc3fd34330
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-04 16:20:35 +0530

    commit

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,5 @@
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(Math);
+// console.log(Math.abs(-4));      //Negative to postive
+// console.log(Math.round(7.3));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(6.9));
