48669e6557de795428ed92025394d9423433d3fa
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-04 14:06:09 +0530

    datatypesSummery

---
+++ dataTypes_summery.js
@@ -0,0 +1,15 @@
+//primitive
+
+//7 types:-String,Number,Boolean,null,undefined,symbol,BigInt
+
+const score=100;
+const scorevalue = 100.3;
+
+const isLoggedIn = false
+const outside = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
