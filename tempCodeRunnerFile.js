3e1f1e8ecfb4ef0d47b620f413644b530f0b573a
Manoranjan dalei <manojdalei9080@gmail.com>
2025-09-05 11:24:43 +0530

    commit

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,4 @@
-console.log(Math);
-// console.log(Math.abs(-4));      //Negative to postive
-// console.log(Math.round(7.3));
-// console.log(Math.ceil(4.7));
-// console.log(Math.floor(6.9));
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
